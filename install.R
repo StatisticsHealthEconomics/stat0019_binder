63f1f524f9b381c302abbe59c3d87d0f691919e1
Gianluca Baio <g.baio@ucl.ac.uk>
2022-01-08 16:00:31 +0000

    Install survHE from CRAN

--- install.R
+++ install.R
@@ -6,6 +6,7 @@ install.packages("R2jags")
 remotes::install_github("giabaio/bmhe_utils")  
 install.packages("rstan",dep=TRUE)
 install.packages("INLA",repos=c(getOption("repos"),INLA="https://inla.r-inla-download.org/R/stable"), dep=TRUE)
-rm(list=ls())
-gc()
-remotes::install_github("giabaio/survHE",ref="devel",dep=TRUE)
+# For now only manages to install the CRAN version of survHE
+# That's not ideal as there are some changes in 'devel' that 'main' doesn't have
+# but at least gets it going...
+install.packages("survHE")
