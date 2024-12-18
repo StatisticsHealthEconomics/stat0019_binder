c66e6a3846082988db7a2c521f901bbe913e0152
Gianluca Baio <g.baio@ucl.ac.uk>
2024-12-18 14:33:51 +0000

    Move all the practicals to JAGS

--- practical/02_mcmc/tutorial_BUGS.R
+++ practical/02_mcmc/tutorial_BUGS.R
@@ -1,22 +1,24 @@
-# Uses the package 'here' to set the working directory to the correct one
+#' Uses the package 'here' to set the working directory to the correct one
 setwd(here::here("02_mcmc"))
 
-# Alternatively, you can do this by going on the bottom-right panel, selecting 
-# the tab "Files" (which is the default/first one), then navigate to "practicals", 
-# then "02_mcmc" and then "R-files". Then click on the right-most button
-# "More" and select from the slider menu "Set As Working Directory"
-#
-# Or, you can do this in the R terminal by typing the command
-# setwd("PATH-TO-YOUR-FOLDER")
-# The path changes depending on the operating system. In the case of the 
-# Binder Virtual Machine, this would be
-# setwd("~/practical/02_mcmc/")
-# or in a more verbose way
-# setwd("/home/rstudio/practical/02_mcmc/")
-# (note that '~' is a shortcut for the home folder '/home/rstudio', in this case)
+#' Alternatively, you can do this by going on the bottom-right panel, selecting 
+#' the tab "Files" (which is the default/first one), then navigate to 
+#' "practicals", then "02_mcmc" and then "R-files". Then click on the 
+#' right-most button "More" and select from the slider menu "Set As 
+#' Working Directory"
+#' 
+#' Or, you can do this in the R terminal by typing the command
+#' setwd("PATH-TO-YOUR-FOLDER")
+#' The path changes depending on the operating system. In the case of the 
+#' Binder Virtual Machine, this would be
+#' setwd("~/practical/02_mcmc/")
+#' or in a more verbose way
+#' setwd("/home/rstudio/practical/02_mcmc/")
+#' (note that '~' is a shortcut for the home folder '/home/rstudio', 
+#' in this case)
 
 
-# Creates the list of data
+#' Creates the list of data
 data=list(
 a = 9.2, b = 13.8, # prior parameters
 y = 15,            # number of successes
@@ -24,12 +26,12 @@ m = 20,            # number of trials
 n = 40,            # future number of trials
 ncrit = 25)        # critical value of future successes
 
-# First load the 'R2OpenBUGS' package
-library(R2OpenBUGS)
+# First load the 'R2jags' package
+library(R2jags)
 
-# Then calls the function 'bugs' to call 'BUGS' in the background
-# and perform the MCMC analysis
-model = bugs(
+#' Then calls the function 'jags' to call 'JAGS' in the background
+#' and perform the MCMC analysis
+model = jags(
   data=data,
   parameters.to.save=c("y.pred","theta","P.crit"),
   inits=NULL,
@@ -42,71 +44,72 @@ model = bugs(
   DIC=TRUE
 )
 
-
-## Or can use a R function to define the model code
+#' Or can use a R function to define the model code
 model.code=function(){
   theta ~ dbeta(a,b)                   # prior distribution
   y ~ dbin(theta,m)                    # sampling distribution
   y.pred ~ dbin(theta,n)               # predictive distribution
   P.crit <- step(y.pred - ncrit + 0.5) # =1 if y.pred >= ncrit,
 }
-model <- bugs(
+model = jags(
   data=data,parameters.to.save=c("y.pred","theta","P.crit"),
   inits=NULL,n.chains=2,n.iter=5000,n.burnin=0,DIC=TRUE,
   # This specifies the model code as the function 'model.code'
   model.file=model.code,
 )
 
-# Visualise the results
-print(model$summary,digit=3)
+#' Visualise the results
+print(model$BUGSoutput$summary,digit=3)
 
-# And uses bmhe functions to graphically show convergence etc
+#' And uses bmhe functions to graphically show convergence etc
 bmhe::traceplot(model,"theta")
 bmhe::traceplot(model,"y.pred")
- 
-# Checks for autocorrelation
-acf(model$sims.list$theta,lwd=4,main="theta",col="red")
-acf(model$sims.list$y.pred,lwd=4,main="y.pred",col="red")
+
+#' Checks for autocorrelation
+bmhe::acfplot(model,parameter="theta",col="red")
+bmhe::acfplot(model,parameter="y.pred",col="red")
 bmhe::diagplot(model,what="Rhat")
 bmhe::diagplot(model,what="n.eff")
 
-# Plots the posterior distributions
+#' Plots the posterior distributions
 bmhe::posteriorplot(model,plot="bar")
 
-# Manually computes PSR (up to correction factor!) and n_eff using the simulations from the model
-# Use the first node
-x=model$sims.array[,,1]
+#' Manually computes PSR (up to correction factor!) and n_eff using the 
+#' simulations from the model
+#' Use the first node
+x=model$BUGSoutput$sims.array[,,1]
 
-# Defines the relevant quantities
-# Number of simulations (rows of x)
+#' Defines the relevant quantities
+#' Number of simulations (rows of x)
 nsims=nrow(x)
-# Number of chains (columsn of x)
+#' Number of chains (columsn of x)
 nchains=ncol(x)
-# Vector of variance for simulated values in each chain
+#' Vector of variance for simulated values in each chain
 sigma2.hat=apply(x,2,var)
-# Vector of mean for simulated values in each chain
+#' Vector of mean for simulated values in each chain
 mu.hat=apply(x,2,mean)
 W=mean(sigma2.hat)
 B=nsims*var(mu.hat)
 
-# Now can compute (approximated) PSR (Rhat) and neff
-Rhat_approx=sqrt( (1/W) * ( ((nsims-1)/nsims) * W + ((nchains+1)/(nsims*nchains)) * B ))
+#' Now can compute (approximated) PSR (Rhat) and neff
+Rhat_approx=sqrt(
+  (1/W) * ( ((nsims-1)/nsims) * W + ((nchains+1)/(nsims*nchains)) * B )
+)
 neff=nchains * nsims * min((1/B)*(((nsims-1)/nsims) * W + B/nsims), 1)
 
-
-# NB: If you like, you can use jags, rather than BUGS. 
-# The process is *almost* identical... 
-#
-# First, load the package 'R2jags' (instead of 'R2OpenBUGS')
-library(R2jags)
-#
-# In the Binder VM, this is already installed, so you are good to go.
-# If you're doing this on your own machine, you must install first
-# jags and then in R run the following command:
-# install.packages("R2jags")
-#
-# Then substitute the command bugs(...) with the command jags(...), like this
-model2 = jags(
+#' NB: If you like, you can use 'bugs', rather than 'jags'. 
+#' The process is *almost* identical... 
+#' 
+#' First, load the package 'R2OpenBUGS' (instead of 'R2jags')
+library(R2OpenBUGS)
+#'
+#' In the Binder VM, this is already installed, so you are good to go.
+#' If you're doing this on your own machine, you must install first
+#' BUGS and then in R run the following command:
+#' install.packages("R2OpenBUGS")
+#' 
+#' Then substitute the command jags(...) with the command bugs(...), like this
+model2 = bugs(
   data=data,
   parameters.to.save=c("y.pred","theta","P.crit"),
   inits=NULL,
@@ -118,25 +121,26 @@ model2 = jags(
   n.burnin=0,
   DIC=TRUE
 )
-#
-# (I'm saving the output as 'model2' as I want to compare with the BUGS
-# output. The name is irrelevant and you could choose whatever you want...).
-#
-# Notice that the two objects 'model' (saved by BUGS) and 'model2' (saved by jags)
-# are *very* similar, but not identical...
-#
-# These commands show what's inside each of the two model outputs
+#'
+#' (I'm saving the output as 'model2' as I want to compare with the BUGS
+#' output. The name is irrelevant and you could choose whatever you want...).
+#'
+#' Notice that the two objects 'model' (saved by JAGS) and 'model2' 
+#' (saved by BUGS) are *very* similar, but not identical...
+#'
+#' These commands show what's inside each of the two model outputs
 names(model)
 names(model2)
-#
-# As you can see, these are slightly different. Jags stores the results generated
-# by running the MCMC algorithm in a sub-object, called 'BUGSoutput'. 
-# So if you want to access the variables stored in there, you need to do it
-# slightly differently than shown above for BUGS. So instead of 
-acf(model$sims.list$theta,lwd=4,main="theta",col="red")
-# 
-# you need to call the slightly revised command
-acf(model2$BUGSoutput$sims.list$theta,lwd=4,main="theta",col="red")
-#
-# See also https://egon.stats.ucl.ac.uk/static/stat0019/practical/02_mcmc/bugs-vs-jags.html
-# for more (minor) differences between BUGS and jags
+#'
+#' As you can see, these are slightly different. Jags stores the results 
+#' generated by running the MCMC algorithm in a sub-object, called 
+#' 'BUGSoutput'. So if you want to access the variables stored in there, 
+#' you need to do it slightly differently than shown above for BUGS. So 
+#' instead of 
+model$BUGSoutput$sims.list$theta |> head()
+#' just do
+model2$sims.list$theta |> head()
+
+#' See also 
+#' https://egon.stats.ucl.ac.uk/static/stat0019/practical/02_mcmc/bugs-vs-jags.html
+#' for more (minor) differences between JAGS and BUGS
--- practical/08_mm/MarkovModel1.R
+++ practical/08_mm/MarkovModel1.R
@@ -1,7 +1,7 @@
-# Asthma cost-effectiveness analysis based on a Markov model
-# source: Price & Briggs (2002); BMHE (5.4)
-
-# Load all the relevant data
+#' Asthma cost-effectiveness analysis based on a Markov model
+#' source: Price & Briggs (2002); BMHE (5.4)
+#' 
+#' Load all the relevant data
 S <- 5			# number of clinical states
 			# s=1: STW
 			# s=2: UTW
@@ -11,7 +11,7 @@ S <- 5			# number of clinical states
 
 J <- 12 		# number of weeks in the follow up
 
-# Observed data on transitions among the states for the two treatments
+#' Observed data on transitions among the states for the two treatments
 r.0 <- (matrix(c(
 66,32,0,0,2, 
 42,752,0,5,20, 
@@ -26,142 +26,185 @@ r.1 <- (matrix(c(
 0,0,0,0,0,
 0,0,0,0,81),c(S,S),byrow=TRUE))
 
-# Includes only the first three rows of data (Hex has no data and TP is modelled deterministically)
+#' Includes only the first three rows of data (Hex has no data and 
+#' TP is modelled deterministically)
 r.0 <- r.0[1:3,]
 r.1 <- r.1[1:3,]
 
 n.0 <- apply(r.0,1,sum)		# total number of patients in each state for t=0
 n.1 <- apply(r.1,1,sum)		# total number of patients in each state for t=1
 
-# Parameters of the Dirichlets prior distributions
-scale <- 10			# scale factor to use different version of minimally informative prior
-				# Reducing this makes the prior less and less informative
+#' Parameters of the Dirichlets prior distributions
+#' scale factor to use different version of minimally informative prior
+#' Reducing this makes the prior less and less informative
+scale <- 10
 alpha.0 <- rep(scale,S)	
 alpha.1 <- rep(scale,S)
 
-# Run the MCMC model
-library(R2OpenBUGS)
-dataBugs <- list("n.0","n.1","r.0","r.1","alpha.0","alpha.1","S") 
+#' Run the MCMC model
+library(R2jags)
+#' Defines the data and points to the model code
+dataJags <- list("n.0","n.1","r.0","r.1","alpha.0","alpha.1","S")
 filein <- here::here("08_mm","MarkovModel1.txt")
+#'
+#' Defines the model parameters and then the inital values
 params <- c("lambda.0","lambda.1")
-# lambda.0 and lambda.1 need: 4 random rows (even Hex has random parameters) 
-#	                      1 row of NAs (because TP is deterministic)
 inits <- function(){
 	temp.0 <- matrix(rgamma(4*S,scale,1),4,S)
-	sum.temp.0 <- apply(temp.0,1,sum)
-	mat.0 <- temp.0/sum.temp.0
-	temp.1 <- matrix(rgamma(4*S,scale,1),4,S)
-	sum.temp.1 <- apply(temp.1,1,sum)
-	mat.1 <- temp.1/sum.temp.1 
-	list(lambda.0=rbind(mat.0,rep(NA,S)),lambda.1=rbind(mat.1,rep(NA,S)))
+  sum.temp.0 <- apply(temp.0,1,sum)
+  mat.0 <- temp.0/sum.temp.0
+  temp.1 <- matrix(rgamma(4*S,scale,1),4,S)
+  sum.temp.1 <- apply(temp.1,1,sum)
+  mat.1 <- temp.1/sum.temp.1
+  list(lambda.0=rbind(mat.0,rep(NA,S)),lambda.1=rbind(mat.1,rep(NA,S)))
 }
+#' NB: lambda.0 and lambda.1 need: 4 random rows (even Hex has 
+#' random parameters) 1 row of NAs (because TP is deterministic)
 
+#' Number of simulations, burnin and thinning
 n.iter <- 10000
 n.burnin <- 5000
 n.thin <- floor((n.iter-n.burnin)/500)
-mm1 <- bugs(data=dataBugs,inits=inits,parameters.to.save=params,model.file=filein,
-	n.chains=2, n.iter, n.burnin, n.thin, DIC=TRUE)
-print(mm1,digits=3)
-attach.bugs(mm1)
 
-## Alternatively, could use JAGS --- code basically identical
-## library(R2jags)
-## dataJags <- list("n.0","n.1","r.0","r.1","alpha.0","alpha.1","S") 
-## filein <- here::here("09_mm","MarkovModel1.txt")
-## params <- c("lambda.0","lambda.1")
-## # lambda.0 and lambda.1 need: 4 random rows (even Hex has random parameters) 
-## #	                      1 row of NAs (because TP is deterministic)
-## inits <- function(){
-## 	temp.0 <- matrix(rgamma(4*S,scale,1),4,S)
-##	sum.temp.0 <- apply(temp.0,1,sum)
-##	mat.0 <- temp.0/sum.temp.0
-##	temp.1 <- matrix(rgamma(4*S,scale,1),4,S)
-##	sum.temp.1 <- apply(temp.1,1,sum)
-##	mat.1 <- temp.1/sum.temp.1 
-##	list(lambda.0=rbind(mat.0,rep(NA,S)),lambda.1=rbind(mat.1,rep(NA,S)))
-## }
-##
-## n.iter <- 10000
-## n.burnin <- 5000
-## n.thin <- floor((n.iter-n.burnin)/500)
-## mm1 <- jags(data=dataJags,inits=inits,parameters.to.save=params,model.file=filein,
-##	n.chains=2, n.iter, n.burnin, n.thin, 
-##	DIC=TRUE, working.directory=working.dir, progress.bar="text")
-## print(mm1,digits=3,intervals=c(0.025, 0.975))
-## attach.jags(mm1)
+#' Finally runs the model using JAGS & prints the results
+mm1 <- jags(
+  data=dataJags, inits=inits, parameters.to.save=params, model.file=filein,
+  n.chains=2, n.iter=n.iter, n.burnin=n.burnin, n.thin=n.thin, DIC=TRUE
+)
+print(mm1,digits=3,intervals=c(0.025, 0.975))
 
-# Now run the Markov model from R
-start <- c(1,0,0,0,0)			# NB analysis for 1 single patient!
-					# (can create a "virtual" population of N individuals
-					#  and allocate them across the states at time j=0)
-# Markov transitions
+attach.jags(mm1)
+ 
+#' ## Alternatively, could use JAGS --- code basically identical
+# library(R2OpenBUGS)
+# dataBugs <- list("n.0","n.1","r.0","r.1","alpha.0","alpha.1","S")
+# filein <- here::here("08_mm","MarkovModel1.txt")
+# params <- c("lambda.0","lambda.1")
+# # lambda.0 and lambda.1 need: 4 random rows (even Hex has random parameters)
+# #	                      1 row of NAs (because TP is deterministic)
+# inits <- function(){
+# 	temp.0 <- matrix(rgamma(4*S,scale,1),4,S)
+# 	sum.temp.0 <- apply(temp.0,1,sum)
+# 	mat.0 <- temp.0/sum.temp.0
+# 	temp.1 <- matrix(rgamma(4*S,scale,1),4,S)
+# 	sum.temp.1 <- apply(temp.1,1,sum)
+# 	mat.1 <- temp.1/sum.temp.1
+# 	list(lambda.0=rbind(mat.0,rep(NA,S)),lambda.1=rbind(mat.1,rep(NA,S)))
+# }
+# 
+# n.iter <- 10000
+# n.burnin <- 5000
+# n.thin <- floor((n.iter-n.burnin)/500)
+# mm1 <- bugs(data=dataBugs,inits=inits,parameters.to.save=params,model.file=filein,
+# 	n.chains=2, n.iter, n.burnin, n.thin, DIC=TRUE)
+# print(mm1,digits=3)
+# attach.bugs(mm1)
+
+
+#' Now run the Markov model from R
+#' NB: this is an analysis for 1 single patient, but can create a "virtual" 
+#'     population of N individuals and allocate them across the states at 
+#'     time j=0
+start <- c(1,0,0,0,0)
+
+#' Markov transitions
 m.0 <- m.1 <- array(NA,c(n.sims,S,(J+1)))
 for (s in 1:S){
 	m.0[,s,1] <- start[s]
 	m.1[,s,1] <- start[s]
 }
 
-# NB: BUGS output the matrices lambda.0 and lambda.1 with simulations for the "random" part
-#     ie only the first 4 rows, as the last one is deterministically defined as c(0,0,0,0,1)
-#     because once a patient is in TF, they can't move away. So need to reconstruct a full
-#     matrix with S rows and S columns for each MCMC simulations. We do this by defining 
-#     new arrays lam0 and lam1 and then stacking up the simulated values for the first (S-1)
-#     rows saved in lambda.0[i,,] and lambda.1[i,,] for MCMC simulations i with a row vector
-#     containing (S-1) 0s and then a 1, ie c(0,0,0,0,1)
-lam0=lam1=array(NA,c(n.sims,S,S))
+#' When using JAGS, your output lambda.0 and lambda.1 will include the last 
+#' row of the transition matrices for each simulation (so all the transitions
+#' off the absorbing state, whose probabilities are 0 to go away and 1 to stay, 
+#' with no variance whatsoever). So we can create the Markov model rather simply
+#' as with the following code
 for (i in 1:n.sims) {
-  lam0[i,,]=rbind(lambda.0[i,,],c(0,0,0,0,1))
-  lam1[i,,]=rbind(lambda.1[i,,],c(0,0,0,0,1))
-	for (j in 2:(J+1)){
-		for (s in 1:S){
-		   # Use the new matrices lam0 and lam1 to do the matrix multiplication
-			m.0[i,s,j] <- sum(m.0[i,,j-1]*lam0[i,,s])
-			m.1[i,s,j] <- sum(m.1[i,,j-1]*lam1[i,,s])
-		}
-	}
-}
-# NB: If you are using JAGS, however, your output lambda.0 and lambda.1 will include the 
-#     last row of the transition matrices for each simulation (so all the transitions off
-#     the absorbing state, whose probabilities are 0 to go away and 1 to stay, with no
-#     variance whatsoever). So if you have used JAGS, all you need to do is the following
-for (i in 1:n.sims) {
-	for (j in 2:(J+1)){
-		for (s in 1:S){
-		   # Use the new matrices lam0 and lam1 to do the matrix multiplication
-			m.0[i,s,j] <- sum(m.0[i,,j-1]*lambda.0[i,,s])
-			m.1[i,s,j] <- sum(m.1[i,,j-1]*lambda.1[i,,s])
-		}
-	}
+  for (j in 2:(J+1)){
+    for (s in 1:S){
+      # Use the new matrices lam0 and lam1 to do the matrix multiplication
+      m.0[i,s,j] <- sum(m.0[i,,j-1]*lambda.0[i,,s])
+      m.1[i,s,j] <- sum(m.1[i,,j-1]*lambda.1[i,,s])
+    }
+  }
 }
 
-# Barplot of the number of people in each state @ each time point
-par(mfrow=c(1,2))
-barplot(apply(m.0,c(2,3),sum),names.arg=seq(0,12),space=.2,xlab="Virtual follow up",
-	ylab="Proportion of patients in each state",main="FP")
+#' NB: Conversely, BUGS output the matrices lambda.0 and lambda.1 with 
+#'     simulations for the "random" part, ie only the first 4 rows, as the 
+#'     last one is deterministically defined as c(0,0,0,0,1) because once 
+#'     a patient is in TF, they can't move away. So need to reconstruct a 
+#'     full matrix with S rows and S columns for each MCMC simulations. 
+#'     We do this by defining new arrays lam0 and lam1 and then stacking up the 
+#'     simulated values for the first (S-1) rows saved in lambda.0[i,,] 
+#'     and lambda.1[i,,] for MCMC simulations i with a row vector containing 
+#'     (S-1) 0s and then a 1, ie c(0,0,0,0,1)
+# lam0=lam1=array(NA,c(n.sims,S,S))
+# for (i in 1:n.sims) {
+#   lam0[i,,]=rbind(lambda.0[i,,],c(0,0,0,0,1))
+#   lam1[i,,]=rbind(lambda.1[i,,],c(0,0,0,0,1))
+# 	for (j in 2:(J+1)){
+# 		for (s in 1:S){
+# 		   # Use the new matrices lam0 and lam1 to do the matrix multiplication
+# 			m.0[i,s,j] <- sum(m.0[i,,j-1]*lam0[i,,s])
+# 			m.1[i,s,j] <- sum(m.1[i,,j-1]*lam1[i,,s])
+# 		}
+# 	}
+# }
 
-barplot(apply(m.1,c(2,3),sum),names.arg=seq(0,12),space=.2,xlab="Virtual follow up",
-	ylab="Proportion of patients in each state",main="SFC")
 
 
-# Now runs the economic analysis
-# Defines the costs
+#' Barplot of the number of people in each state at each time point
+#' First gets the aggregated data from the simulations and then 
+#' wrangles the resulting dataset to a long format. This plot is often 
+#' termed a "Markov trace"
+apply(m.0,c(2,3),sum) |> t() |> as_tibble() |>  
+  setNames(c("STW","UTW","Pex","Hex","TF")) |> 
+  mutate(Intervention="FP",time=seq(0,12)) |> 
+  bind_rows(
+    apply(m.1,c(2,3),sum) |> t() |> as_tibble() |>  
+      setNames(c("STW","UTW","Pex","Hex","TF")) |> 
+      mutate(Intervention="SFC",time=seq(0,12)) 
+  ) |> 
+  pivot_longer(!c(time,Intervention),names_to="state",values_to="npeople") |>
+  #' Turns the variable 'state' into a factor, which we order according to
+  #' the various states in our Markov model
+  mutate(
+    state=factor(state),
+    state=forcats::fct_relevel(state,c("STW","UTW","Pex","Hex","TF"))
+  ) |>
+  #' Try and run up to here (removing the last pipe '|>' in the above row) to
+  #' check what the resulting data look like...
+  #' 
+  #' Then uses ggplot to make the barplot & customise it...
+  ggplot(aes(x=time,y=npeople,fill=as_factor(state))) + 
+    geom_bar(position="stack",stat="identity") + 
+  xlab("Virtual follow up") + ylab("Proportion of patients in each state") +
+  theme_bw() + facet_wrap(~Intervention) +
+  theme(legend.position = "bottom") + labs(fill="State") + scale_fill_grey()
+
+
+#' Now runs the economic analysis
+#' Defines the costs
 unit.cost.0 <- c(2.38,2.38,1815.58,95.21)
 unit.cost.1 <- c(7.96,7.96,1821.17,100.79)
 
 cost0 <- cost1 <- matrix(NA,n.sims,J)
 for (i in 1:n.sims) {
 	for (j in 2:(J+1)) {
-		cost0[i,j-1] <- m.0[i,S,j]*(unit.cost.0%*%m.0[i,1:(S-1),j])/sum(m.0[i,1:(S-1),j]) + 
+		cost0[i,j-1] <- m.0[i,S,j]*
+		  (unit.cost.0%*%m.0[i,1:(S-1),j])/sum(m.0[i,1:(S-1),j]) + 
 				unit.cost.0%*%m.0[i,1:(S-1),j]
-		cost1[i,j-1] <- m.1[i,S,j]*(unit.cost.0%*%m.0[i,1:(S-1),j])/sum(m.0[i,1:(S-1),j]) + 
+		cost1[i,j-1] <- m.1[i,S,j]*
+		  (unit.cost.0%*%m.0[i,1:(S-1),j])/sum(m.0[i,1:(S-1),j]) + 
 				unit.cost.1%*%m.1[i,1:(S-1),j]
 	}
 }
 
-## General formulation to apply discount
+#' General formulation to apply discount
 delta.b <- 0.035	# discount rate for benefits (3.5%)
 delta.c <- 0.035	# discount rate for costs (3.5%)
-# Defines the discount factors
+
+#' Defines the discount factors
 disc.b <- numeric(); disc.c <- numeric()
 disc.b[1] <- 1; disc.c[1] <- 1
 for (j in 2:J) {
@@ -176,23 +219,22 @@ for (j in 1:J) {
 	disc.eff1[,j] <- m.1[,1,j]/disc.b[j]
 }
 
-# Sums the values across all time points and creates matrix of costs
+#' Sums the values across all time points and creates matrix of costs
 c <- matrix(NA,n.sims,2)
 c[,1] <- apply(cost0,1,sum)
 c[,2] <- apply(cost1,1,sum)
 
-# Effectiveness
+#' Effectiveness
 e <- matrix(NA,n.sims,2)
 e[,1] <- apply(m.0[,1,],1,sum)
 e[,2] <- apply(m.1[,1,],1,sum)
 
 
-# Cost-effectiveness analysis
+#' Cost-effectiveness analysis
 library(BCEA)
 ints <- c("FP","SFC")
 m <- bcea(e,c,ref=2,interventions=ints,Kmax=300)
 
-contour2(m,300)
-plot(m)
+contour2(m,wtp = 300,graph="gg")
+plot(m,graph="gg")
 summary(m)
-
