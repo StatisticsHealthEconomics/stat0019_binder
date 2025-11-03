609a8e6e75832d5b26c575e12d42bc24fd51e6cd
Gianluca Baio <g.baio@ucl.ac.uk>
2025-11-03 17:09:26 +0000

    Use remotes for the non-CRAN packages that would only be on r-universe.dev. They would break mybinder, but work OK from remotes/github

--- install.R
+++ install.R
@@ -15,6 +15,13 @@ install.packages("sn")
 install.packages("voi")
 install.packages("multinma")
 remotes::install_github("giabaio/survHEinla")
+remotes::install_github("StatisticsHealthEconomics/outstandR")
+remotes::install_github("n8thangreen/simcovariates")
+## For some reason, packages that are *only* on r-universe.dev break the mybinder
+## installation... They work OK if installed through remotes. And in fact, other
+## repos don't seem to interact badly with bspm (which looks for packages on r2u
+## and installs it using apt install r-cran-<pkg>). But r-universe.dev has something
+## that messes up with that system...
 #install.packages("outstandR", repos = c("https://statisticshealtheconomics.r-universe.dev", "https://cloud.r-project.org"))
 #install.packages("simcovariates", repos = c("https://n8thangreen.r-universe.dev", "https://cloud.r-project.org"))
 #install.packages("survHEinla", repos = c("https://giabaio.r-universe.dev", "https://cloud.r-project.org"))
